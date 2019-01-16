a38ba078d87dbc3971a9af575125438d909ea955
Namory Traore <namory.traore@ericsson.com>
2019-01-16 11:14:13 -0500

    First commit of ItemManager project

---
+++ src/test/java/com/nexttchnologies/itemManager/ItemManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nexttchnologies.itemManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
